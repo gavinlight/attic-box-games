694f873798e8950ff295bc1262e785de0a44ed80
gavin <gavin.ligthart@labela.nl>
2019-09-20 11:05:05 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React, { lazy, Suspense } from 'react';
+import { Switch, Route, withRouter } from 'react-router-dom';
+import GlobalStyle from 'styles';
+
+const Home = lazy(() => import('modules/Home'));
+
+const App = () => (
+  <main>
+    <GlobalStyle />
+    <Suspense fallback={<span>loading</span>}>
+      <Switch>
+        <Route path="/" component={Home} exact />
+      </Switch>
+    </Suspense>
+  </main>
+);
+
+export default withRouter(App);
---
+++ src/components/modules/Home/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Home = () => <h1>Attic Box Games</h1>;
+
+export default Home;
