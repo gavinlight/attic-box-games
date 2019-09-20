95c9ca9b7d25240da102a357c463a0be82d47974
gavin <gavin.ligthart@labela.nl>
2019-09-20 11:21:18 +0200

    Add base setup for page, header and footer

---
+++ src/components/common/Container/index.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+import { media, sizes } from 'styles/utils';
+
+const Container = styled.div`
+  width: 100%;
+  position: relative;
+  margin: 0 auto;
+  padding: 0 28px;
+
+  ${media.tablet`
+    padding: 0;
+    max-width: ${sizes.tablet}px;
+  `}
+
+  ${media.desktop`
+    max-width: ${sizes.desktop - 50}px;
+  `}
+
+  ${media.large`
+    max-width: ${sizes.large - 50}px;
+  `}
+`;
+
+export default Container;
---
+++ src/components/common/Page/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PT from 'prop-types';
+import styled from 'styled-components';
+
+import Header from 'common/Header';
+import Footer from 'common/Footer';
+
+const StyledPage = styled.div`
+  ${'' /* font-family: ${(props) => props.theme.fonts.sourcesans}; */}
+`;
+
+const Page = ({ children }) => (
+  <StyledPage>
+    <Header />
+    {children}
+    <Footer />
+  </StyledPage>
+);
+
+Page.propTypes = {
+  children: PT.oneOfType([
+    PT.arrayOf(PT.node),
+    PT.node,
+  ]),
+};
+
+export default Page;
