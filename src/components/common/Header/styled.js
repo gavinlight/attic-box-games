f426395c49d329f96d747991e3f216c7a0905fcb
gavin <gavin.ligthart@labela.nl>
2019-09-20 14:11:17 +0200

    Fix header styling so it doesnt stick with scrolling

--- src/components/common/Header/styled.js
+++ src/components/common/Header/styled.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export const HeaderContainer = styled.div`
   display: flex;
   justify-content: space-between;
-  position: fixed;
+  position: absolute;
   top: 0;
   left: 0;
   width: 100%;
