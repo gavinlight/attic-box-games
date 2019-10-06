fca1ab87a81e650f3e126fb0103f4b8130234993
gavin_active <gavin.ligthart@activecollective.nl>
2019-10-06 17:15:46 +0200

    use p5 wrapper to load model

--- src/components/common/Loader/index.jsx
+++ src/components/common/Loader/index.jsx
@@ -1,48 +1,8 @@
 import React from 'react';
-import * as p5 from 'p5';
+import P5Wrapper from 'react-p5-wrapper';
 
-import cube from 'models/cube.obj';
+import sketch from './sketch';
 
-const Loader = () => {
-  const easeInOutQuad = (t) => t < .5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
-
-  const s = (sk) => {
-    let model;
-
-    sk.preload = () => {
-      model = sk.loadModel(cube);
-    }
-
-    sk.setup = () => {
-      sk.createCanvas(window.innerWidth, window.innerHeight, sk.WEBGL);
-    }
-
-    sk.draw = () => {
-      sk.background(242);
-      sk.fill(120);
-      sk.lights();
-
-      sk.rotateY(easeInOutQuad(3 + sk.sin(sk.frameCount * 0.01)));
-      sk.rotateX(sk.radians(55));
-      sk.rotateY(sk.radians(45));
-      sk.rotateZ(sk.radians(0));
-
-      sk.scale(0.8);
-      sk.model(model);
-      sk.normalMaterial();
-    }
-  }
-
-  React.useEffect(() => {
-    const P5 = new p5(s);
-    console.log('init');
-    return () => {
-      console.log('cleanup', P5);
-      P5.remove();
-    }
-  }, []);
-
-  return null;
-};
+const Loader = () => <P5Wrapper sketch={sketch} />;
 
 export default Loader;
