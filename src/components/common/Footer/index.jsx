82c6c9e999ebe8ba27bc43b5a16c50fa85224731
gavin_active <gavin.ligthart@activecollective.nl>
2019-10-21 22:37:01 +0200

    Add reversed anchor

--- src/components/common/Footer/index.jsx
+++ src/components/common/Footer/index.jsx
@@ -8,7 +8,7 @@ import { FooterContainer } from './styled';
 const Footer = () => (
   <FooterContainer>
     <Container>
-      Questions about our team, Seek or anything else? Contact us via <Anchor to="qwerwer">mail</Anchor> or <Anchor to="erewr">Facebook</Anchor>
+      Questions about our team, Seek or anything else? Contact us via <Anchor to="qwerwer" reversed>mail</Anchor> or <Anchor to="erewr" reversed>Facebook</Anchor>
     </Container>
   </FooterContainer>
 );
