dfdca2dce5503637ad7650c808a1bde7366dc167
gavin <gavin.ligthart@labela.nl>
2019-10-22 22:13:34 +0200

    Finish up footer styling

--- src/components/common/Footer/index.jsx
+++ src/components/common/Footer/index.jsx
@@ -9,7 +9,7 @@ import { FooterContainer } from './styled';
 const Footer = () => (
   <FooterContainer>
     <Container>
-      Questions about our team, Seek or anything else?
+      Questions about our team, Seek or anything else? <br />
       Contact us via <Anchor to="qwerwer" reversed>mail</Anchor> or <Anchor to="erewr" reversed>Facebook</Anchor>
       <Logo size="60" />
     </Container>
--- src/components/common/Footer/styled.js
+++ src/components/common/Footer/styled.js
@@ -1,10 +1,23 @@
 import styled from 'styled-components';
 
+import Container from 'common/Container';
+
+import footerSvg from 'vectors/footer.svg?external';
+
 export const FooterContainer = styled.div`
   padding: 80px 0;
-  background-color: ${(props) => props.theme.colors.grey};
   font-size: 30px;
+  line-height: 36px;
   line-height: 46px;
+  background-color: ${(props) => props.theme.colors.grey};
+  background-image: url(${footerSvg});
+  background-size: auto 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+
+  & > ${Container} {
+    padding: 0 40px;
+  }
 
   & img {
     display: block;
