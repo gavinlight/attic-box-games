128fd01f0e63f7eee9acaeddf69c2c7e1d178372
gavin <gavin.ligthart@labela.nl>
2019-10-22 22:42:31 +0200

    Add mobile styling to about page

--- src/components/modules/Home/components/About/styled.js
+++ src/components/modules/Home/components/About/styled.js
@@ -1,20 +1,30 @@
 import styled from 'styled-components';
+import { media } from 'styles/utils';
 
 export const AboutContainer = styled.div`
   display: flex;
-  margin-bottom: 80px;
+  flex-direction: column;
+  margin-bottom: 40px;
+
+  ${media.tablet`
+    flex-direction: row;
+    margin-bottom: 80px;
+  `}
 `;
 
 export const Heading = styled.h3`
-  display: flex;
-  flex-direction: column;
-  flex: 1 0 20%;
   max-width: 100px;
-  text-align: right;
-  margin: 6px 50px 0;
   text-transform: uppercase;
   font-weight: 700;
   font-size: 20px;
+
+  ${media.tablet`
+    display: flex;
+    flex-direction: column;
+    flex: 1 0 20%;
+    margin: 6px 50px 0;
+    text-align: right;
+  `}
 `;
 
 export const Text = styled.div`
